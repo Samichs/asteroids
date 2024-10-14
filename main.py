0d70662f071e3efe86e05a7fdb55a7115bb5b9ed
Kyle Ng <kyleecng@gmail.com>
2024-10-14 15:44:05 -0700

    define rest of main game loop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import sys
 import pygame
 
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print('Starting asteroids!')
@@ -9,6 +14,21 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +36,23 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        for obj in asteroids:
+            if obj.check_collision(player):
+                print('Game over!')
+                sys.exit()
+            for shot in shots:
+                if obj.check_collision(shot):
+                    shot.kill()
+                    obj.split()
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == '__main__':
     main()
